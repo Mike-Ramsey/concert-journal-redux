845870038f07131bcc1671aee67f581cfb54a828
Mike Ramsey <meramsey@gmail.com>
2022-04-17 16:16:28 -0500

    modal opens, need to link concert

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ export default function App() {
   }
 
   const startHandleEdit = (concert) => {
-    setEditConcert(concert);
+    setEditConcert(concert.id);
     setIsEditModalOpen(true);
   }
   
